959f643ce728fb3e14afd0926cb05fc5f11a30b3
Vladimir Revyagin <skorabeyf@gmail.com>
2025-04-21 14:20:36 +0300

    style: установил дефолтные стили

--- src/assets/components/pages/CatsPage.tsx
+++ src/assets/components/pages/CatsPage.tsx
@@ -1,6 +1,7 @@
 import { useFetch } from '../hooks/useFetch'
 import { path } from '../path..ts/path'
 import { StyledCatCard } from '../ui/styledCarCard'
+import './styles.css'
 
 export const CatsPage = () => {
   const { data, error, isLoading } = useFetch(path)
@@ -9,12 +10,15 @@ export const CatsPage = () => {
 
   if (isLoading) return <p>Loading cats...</p>
   return (
-    <div>
+    <div className="app">
       <h1>Cat gallary</h1>
-      <div className="container">
-        {cats.map((cat) => (
-          <StyledCatCard key={cat.id} {...cat} />
-        ))}
+      <div className="cards-container">
+        <h2>Styled Components</h2>
+        <div className="cards-section">
+          {cats.map((cat) => (
+            <StyledCatCard key={cat.id} {...cat} />
+          ))}
+        </div>
       </div>
     </div>
   )
